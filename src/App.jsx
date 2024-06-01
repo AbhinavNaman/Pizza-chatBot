e23724e8918afd2a4d9498394657049cd4cfbb7b
Abhinav Naman <93396109+AbhinavNaman@users.noreply.github.com>
2024-06-01 16:50:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,18 @@ import CardCarousel from './components/CardCarousel';
 import ChatInput from './components/ChatInput';
 import './App.css';
 import OtherOptions from './components/OtherOptions';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div className="mx-32 m-10 font-sans">
+    <>
+    <Navbar/>
+    <div className="mx-32 m-10 font-sans ">
       <ChatInput />
     </div>
+    </>
   );
 };
 
 export default App;
 
-
-
-
-{/* <div className=" flex justify-center font-sans w-full">
-    <div className="mx-20 m-10  flex flex-col gap-8">
-      <h1 className='text-3xl text-center font-semibold'>Copilot</h1>
-      <CardCarousel />
-      <OtherOptions/>
-      <ChatInput />
-    </div>
-    </div> */}
